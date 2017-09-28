19f19beecdc2f452f262d6bf244de12b02aeaa5e
Benjamin Goodwin <goodb25@gmail.com>
2017-09-28 19:30:27 +0200

    corrected Segfault edgeExist, and corrected addEdge

--- src/main.c
+++ src/main.c
@@ -3,11 +3,11 @@
 int main(int argc, char* argv[]) {
   //int quit = 0;
   Graph graph;
-  create_graph(&graph, 10, false);
+  create_graph(&graph, 10, true);
   add_node(&graph, 3);
   add_node(&graph, 5);
   add_edge(&graph, 3, 5, 1, 0);
-  f(is_edge_exists(&graph, 1)){
+  if(is_edge_exists(&graph, 1)){
     printf("Edge 1 exists\n");
   } else {
     printf("Edge 1 doesn't exists\n");
