72a9cee7ea3a77afee7060b62afc32bcbb86ec88
Xeryko <temporos.biohazard@hotmail.fr>
2017-09-28 20:25:28 +0200

    Removing non important stuff in main function

--- src/main.c
+++ src/main.c
@@ -6,17 +6,11 @@
 int main(int argc, char* argv[]) {
   //int quit = 0;
   Graph graph;
-  create_graph(&graph, 10, true);
+  create_graph(&graph, 11, false);
   add_node(&graph, 3);
   add_node(&graph, 5);
-  //add_edge(&graph, 3, 5, 1, 0, false);
+  add_edge(&graph, 3, 5, 1, 0, false);
   view_graph(&graph);
-  /*
-  if(is_edge_exists(&graph, 1)){
-    printf("Edge 1 exists\n");
-  } else {
-    printf("Edge 1 doesn't exists\n");
-  }
 /*
   printHeader();
   while(quit == 0) {
